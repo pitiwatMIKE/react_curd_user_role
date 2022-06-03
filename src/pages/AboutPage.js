92dfdfabb668b0323967e355ee5afff8c400eed4
pitiwatMIKE <pitiwat.pr61@gmail.com>
2022-06-03 17:27:03 +0700

    add about.md

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Markdown from "markdown-to-jsx";
 
 export default function AboutPage() {
+  const fileName = "about.md";
+  const [post, setPost] = useState("");
+
+  useEffect(() => {
+    import(`../markdown/${fileName}`)
+      .then((res) => {
+        fetch(res.default)
+          .then((res) => res.text())
+          .then((res) => setPost(res))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  });
   return (
-    <div>AboutPage</div>
-  )
+    <>
+      <Markdown>{post}</Markdown>
+    </>
+  );
 }
